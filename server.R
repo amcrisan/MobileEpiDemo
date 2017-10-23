9bbb398a21ec0296e0614933cbf8200761e02bb3
amcrisan <anamaria.crisan@bccdc.ca>
2017-10-23 08:41:45 -0700

    implementing interactive features

--- server.R
+++ server.R
@@ -1,23 +1,87 @@
+library(dplyr)
+library(tidyr)
+library(igraph)
+library(shinydashboard)
+library(ggplot2)
+library(lubridate)
 
-# This is the server logic for a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
+#Loading the data (done ahead of every session)
+zombDat<-readRDS(file="./data/zombDat.RDS")
+connections<-readRDS(file="./data/patientConnects.RDS")
+g<-readRDS(file="./data/networkGraph.RDS")
+exposure<-(file="./data/exposureDat.RDS")
 
-library(shiny)
+description<-"Showing: Patient exposure timing"
+
+#######################
+# Shiny server code
 
 shinyServer(function(input, output) {
 
-  output$distPlot <- renderPlot({
-
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+  output$timeline<-renderPlot({
+    #timeline of zombie outbreak
+    pBase<-ggplot(data=zombDat, aes(x = dateOfExposureStart,y=ID,group=ID))+
+      scale_y_continuous(breaks=0:24)+
+      scale_x_date(date_breaks = "1 week", date_minor_breaks = "1 days",date_labels="%b-%d")+
+      ylab("Patient ID")+
+      theme_bw()+
+      theme(legend.position="below")+
+      theme(axis.text = element_text(size=12),
+            axis.text.x = element_text(angle=90,vjust=0.5),
+            axis.title.x = element_blank(),
+            panel.grid.major.x = element_line(size=2))
+    
+    
+    #Baseplot is tile data
+    if(!is.null(input$colorData)){
+      pBase<-pBase +
+        geom_tile(aes_string(fill=input$colorData),colour="white",alpha=0.7)
+    }else{
+      pBase<-pBase +
+        geom_tile(fill = "darkgray",colour="white")
+    }
+    
+    ###########################
+    # Adding data to graph
+    ###########################
+    
+    #adding exposure tiles
+    if(!is.null(input$addData)){
+      #add date of symptom onset
+      if("Symptom Onset" %in% input$addData){
+        if(!is.null(input$colorData)){
+          pBase<-pBase+
+            geom_segment(aes(x=dateOfExposureEnd,y=ID,xend=dateOfOnset,yend=ID,group=ID),colour="grey")+
+            geom_point(aes_string(x="dateOfOnset",y="ID",fill=input$colorData),colour="black",size=3,pch=21)
+        }else{
+          pBase<-pBase+
+            geom_segment(aes(x=dateOfExposureEnd,y=ID,xend=dateOfOnset,yend=ID,group=ID),colour="grey")+
+            geom_point(aes(x=dateOfOnset,y=ID),colour="black",fill="red",size=3,pch=21)
+        }
+      }
+      
+      #add the network transmission
+      if("Patient Relationships" %in% input$addData){
+        #if exposure is on
+        pBase<-pBase +
+          geom_curve(data=connections,aes(x=sourceEnd,y=source,xend=targetStart,yend=ID),colour="black",curvature=-0.15,alpha=0.75)
+      }
+    }
+    
+    #plot the plot!
+    plot(pBase + theme(legend.position="bottom"))
+      
   })
-
+  
+  output$textDesc<-renderText({
+    textBase<-"The current graph shows the dates a patient was potentially exposed to the zombie (square blocks)."
+  
+    #color
+    if(!is.null(input$colorData)){
+      textBase<-paste(textBase,sprintf("The data points are coloured by patient's %s", input$colorData))
+    }
+    
+  })
+  
+  #changing the content of the filter menu by selection
 })
--- ui.R
+++ ui.R
@@ -1,30 +1,53 @@
-
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
 library(shiny)
+library(shinydashboard)
+library(shinythemes)
 
-shinyUI(fluidPage(
 
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
+header <- dashboardHeader(
+  title = "Zombie Outbreak Timeline",
+  titleWidth = 450
+)
 
-  # Sidebar with a slider input for number of bins
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-    ),
-
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("distPlot")
-    )
+body<-dashboardBody(
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "style.css")
+  ),
+  #menu items
+  box(title="Graph Options",
+      status="info",
+      solidHeader = TRUE,
+      width=NULL,
+      collapsible = TRUE,
+      collapsed = TRUE,
+      div(
+        HTML("<em><strong>Usage Tip:</strong> You can manipulate the appearance of the graphic below through the menu above</em>")
+      ),
+      br(),
+      tabsetPanel(
+        tabPanel(title="Add data",
+                 hr(),
+                 div(HTML("You can <em>add</em> data to the basetimeline graphic. Select what data to add using the check boxes.")),
+                 checkboxGroupInput("addData",choices=c("Symptom Onset","Patient Relationships"),label=NULL)
+                 ),
+        tabPanel(title ="Colour by",
+                 hr(),
+                 div(HTML("You can <em>change the colour</em> of data points. You can only colour data using one other data type. Use the checkboxes below to select which data point the colour by.")),
+                 selectInput("colorData",choices=c("Status"="status","Sex" = "Sex","Occupation"="Occupation"),label=NULL,multiple=FALSE)),
+        tabPanel(title= "Filter by"),
+        type="pills"
+      )
+  ),
+  fluidRow(
+    plotOutput("timeline"),
+    br(),
+    div(HTML("<strong>Description of the plot:</strong>")),
+    textOutput("textDesc")
   )
-))
+)
+dashboardPage(
+  header,
+  dashboardSidebar(disable = TRUE),
+  body,
+  skin="black"
+)
+  
